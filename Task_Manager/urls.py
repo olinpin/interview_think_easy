0efc2d3c100be28ebf9b361be5e7663bbbc0a189
Oliver <Oliver.Hnat@gmail.com>
2021-12-22 10:05:32 +0100

    Added login and logout views, css

--- Task_Manager/urls.py
+++ Task_Manager/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks"
+
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('login', views.login_view, name="login"),
+    path('logout', views.logout_view, name="logout"),
 ]
--- Task_Manager/views.py
+++ Task_Manager/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth import login, logout, authenticate
+from django.urls import reverse
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'Task_Manager/index.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        # get the username and password
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # make sure they are correct
+        user = authenticate(request, username=username, password=password)
+        # if credentials are correct, log user in
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('tasks:index'))
+        else:
+            # invalid credentials
+            return render(request, "Task_Manager/login.html", {
+                "message": "Invalid credentials",
+                "bad": "yes"
+            })
+    return render(request, "Task_Manager/login.html")
+
+
+def logout_view(request):
+    # logs user out
+    logout(request)
+    return render(request, "Task_Manager/login.html",{
+        "message": "You've been logout.",
+        "good": "yes",
+    })
